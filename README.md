bf3f8edbe5fcaffef77b5aa2f54ff5d3f8ae4434
Nikhil Raj <nikhil25803@gmail.com>
2022-06-09 02:58:47 +0530

    Added greet function with voice

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# voice-assistant
-Python voice assistant
+# Python voice assistant
+
+
+> ## Setup
+
+---
+
+### Step 1 : Clone the repository
+```bash
+git clone https://github.com/nikhil25803/voice-assistant.git
+```
+
+### Step 2 : Activate the virtual enviroment
+```bash
+venv\Scripts\Activate.ps1 
+```
+
+### Step 3 : Intall packages
+```bash
+pip install -r requirements.txt
+```
---
+++ features/greet.py
@@ -0,0 +1,42 @@
+import pyttsx3
+from datetime import datetime
+
+
+class Greet:
+
+    def greet(self):
+
+        engine = pyttsx3.init()
+
+        # Volume
+        volume = engine.getProperty('volume')
+        engine.setProperty('volume', 1.0)
+
+        # Voice
+        voices = engine.getProperty('voices')
+        engine.setProperty('voice', voices[1].id)
+
+        # Time
+        time_hour = datetime.now().hour
+        if (time_hour >= 0 or time_hour <= 12):
+            # Greet
+
+            engine.say('Good Morning ! How may I help you?')
+            engine.runAndWait()
+            engine.stop()
+        elif (time_hour > 12 or time_hour <= 5):
+            # Greet
+
+            engine.say('Good Morning ! How may I help you?')
+            engine.runAndWait()
+            engine.stop()
+        else:
+            # Greet
+
+            engine.say('Good Morning ! How may I help you?')
+            engine.runAndWait()
+            engine.stop()
+
+
+person = Greet()
+person.greet()
